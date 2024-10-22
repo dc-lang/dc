d309ba325555c539c36815e6512c07d67f7eb942
aceinetx <mouselin82@gmail.com>
2024-10-22 10:25:00 +0300

    update workflow

--- toolchain-arm.cmake
+++ toolchain-arm.cmake
@@ -3,9 +3,10 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
+# Specify the Clang compiler
 set(CMAKE_C_COMPILER clang-19)
 set(CMAKE_CXX_COMPILER clang++-19)
 
-# Specify the ARM target
+# Specify the target architecture
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=arm-linux-gnueabi")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --target=arm-linux-gnueabi")
