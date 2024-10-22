bd831325b0967ae1a36f74d662981b1d7ac47385
aceinetx <mouselin82@gmail.com>
2024-10-22 10:19:28 +0300

    update workflow

--- toolchain-arm.cmake
+++ toolchain-arm.cmake
@@ -5,5 +5,5 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # Specify the cross compiler
-set(CMAKE_C_COMPILER arm-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnu-g++)
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
