632c608b4fb929598f4d3df39361cde423221962
aceinetx <mouselin82@gmail.com>
2024-10-22 10:22:48 +0300

    update workflow

--- toolchain-arm.cmake
+++ toolchain-arm.cmake
@@ -1,9 +1,11 @@
 # toolchain-arm.cmake
 
-# Set the C and C++ compilers
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# Specify the cross compiler
-set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
+set(CMAKE_C_COMPILER clang-19)
+set(CMAKE_CXX_COMPILER clang++-19)
+
+# Specify the ARM target
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=arm-linux-gnueabi")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --target=arm-linux-gnueabi")
